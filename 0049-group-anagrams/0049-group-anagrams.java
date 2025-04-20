38de64454a1d33cba0cee99b0e052d5b2e805fb7
Dheeraj Gedupudi <102639070+DheerajGedupudi@users.noreply.github.com>
2025-04-20 14:32:20 -0500

    Time: 529 ms (5.08%), Space: 47.7 MB (82.78%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,32 +1,52 @@
 class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) 
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<Integer[]> freqList = new ArrayList<>();
+        List<List<String>> result = new ArrayList<>();
+        int n = strs.length;
+        for (int i=0; i<n; i++)
+        {
+            Integer[] freq = new Integer[26];
+            Arrays.fill(freq, 0);
+            for (char c : strs[i].toCharArray())
+            {
+                freq[c-'a']++;
+            }
+            int foundIndex = -1;
+            for (int j=0; j<freqList.size(); j++)
+            {
+                if (equals(freq, freqList.get(j)))
+                {
+                    foundIndex = j;
+                }
+            }
+            if (foundIndex==-1)
+            {
+                freqList.add(freq);
+                result.add(new ArrayList<>());
+                foundIndex = freqList.size()-1;
+            }
+            result.get(foundIndex).add(strs[i]);
+        }
+        return result;
+    }
+
+    private boolean equals(Integer[] a, Integer[] b)
     {
-        HashMap<String, ArrayList<String>> map = new HashMap<>();
-        List<List<String>> ans = new ArrayList<>();
-        for (String s : strs)
+        for (int i=0; i<26; i++)
         {
-            // ArrayList<Integer> ascii = new ArrayList<>();
-            // for (int i=0; i<26; i++)
-            // {
-            //     ascii.add(0);
-            // }
-            // for (char c : s.toCharArray())
-            // {
-            //     int freq = ascii.get(c-'a');
-            //     ascii.set((c-'a'),freq+1);
-            // }
-            char[] arr = s.toCharArray();
-            Arrays.sort(arr);
-            String sorted = new String(arr);
-            ArrayList<String> list = map.getOrDefault(sorted, new ArrayList<String>());
-            list.add(s);
-            map.put(sorted, list);
+            if (a[i]!=b[i])
+            {
+                return false;
+            }
         }
-        // System.out.println(map);
-        for (Map.Entry<String, ArrayList<String>> m : map.entrySet())
-        {
-            ans.add(m.getValue());
-        }
-        return ans;
+        return true;
     }
 }
+
+/*
+
+
+int[]  count
+List<int[]>
+
+*/
