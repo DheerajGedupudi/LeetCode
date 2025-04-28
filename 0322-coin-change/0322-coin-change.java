96457f559db6a7358c7171737eae2662c136b3fe
Dheeraj Gedupudi <102639070+DheerajGedupudi@users.noreply.github.com>
2025-04-28 14:33:12 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.java
@@ -0,0 +1,85 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int INF = Integer.MAX_VALUE;
+        Arrays.sort(coins);
+        int n = coins.length;
+        int[][] dp = new int[n+1][amount+1];
+        for (int i=0; i<=n; i++)
+        {
+            for (int j=0; j<=amount; j++)
+            {
+                dp[i][j] = Integer.MAX_VALUE;
+            }
+        }
+        for (int i=0; i<=n; i++)
+        {
+            dp[i][0] = 0;
+        }
+        for (int i=1; i<=n; i++)
+        {
+            int coin = coins[i-1];
+            for (int j=1; j<coin && j<=amount; j++)
+            {
+                dp[i][j] = dp[i-1][j];
+            }
+            for (int j=coin; j<=amount; j++)
+            {
+                if (dp[i][j-coin]!=INF)
+                {
+                    dp[i][j] = Math.min(dp[i][j], dp[i][j-coin]+1);
+                }
+            }
+            // print(dp);
+        }
+        int min = INF;
+        for (int i=0; i<=n; i++)
+        {
+            min = Math.min(min, dp[i][amount]);
+        }
+        if (min==INF)
+        {
+            return -1;
+        }
+        return min;
+    }
+
+    private void print(int[][] grid)
+    {
+        for (int[] row : grid)
+        {
+            for (int c  :row)
+            {
+                if (c==Integer.MAX_VALUE)
+                {
+                    System.out.print("INF, ");
+                }
+                else
+                {
+                    System.out.print(c+", ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
+
+
+
+/*
+
+coins
+[1,2,5]
+
+amount
+
+1 -> 11
+
+
+. . 1 2 3 4 5 6 7 8 9 10 11
+-----------------------------
+1 0 1 2 3 4 5 6 7 8 9 10 11
+2 0 1 1 2 2 3 3 4 4 5 5  6
+5 0 1 1 2 2 1 2 2 
+
+*/
