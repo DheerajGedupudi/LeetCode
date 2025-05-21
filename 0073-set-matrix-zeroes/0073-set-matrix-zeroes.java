94e6e173b3b41606dc0ead424ae87ab73eabd001
Dheeraj Gedupudi <102639070+DheerajGedupudi@users.noreply.github.com>
2025-05-20 20:00:35 -0500

    Time: 1 ms (72.83%), Space: 45.8 MB (29.76%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -2,38 +2,73 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int n = matrix.length;
         int m = matrix[0].length;
-        boolean[] rows = new boolean[n];
-        boolean[] cols = new boolean[m];
+        boolean firstRow = false;
+        boolean firstCol = false;
         for (int i=0; i<n; i++)
         {
             for (int j=0; j<m; j++)
             {
                 if (matrix[i][j]==0)
                 {
-                    rows[i] = true;
-                    cols[j] = true;
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                    if (i==0)
+                    {
+                        firstRow = true;
+                    }
+                    if (j==0)
+                    {
+                        firstCol = true;
+                    }
                 }
             }
         }
-        for (int i=0; i<n; i++)
+        for (int i=1; i<n; i++)
         {
-            if (rows[i])
+            if (matrix[i][0]==0)
             {
-                for (int j=0; j<m; j++)
+                for (int j=1; j<m; j++)
                 {
                     matrix[i][j] = 0;
                 }
             }
         }
-        for (int i=0; i<m; i++)
+        for (int j=1; j<m; j++)
         {
-            if (cols[i])
+            if (matrix[0][j]==0)
             {
-                for (int j=0; j<n; j++)
+                for (int i=1; i<n; i++)
                 {
-                    matrix[j][i] = 0;
+                    matrix[i][j] = 0;
                 }
             }
         }
+        if (firstRow)
+        {
+            for (int j=0; j<m; j++)
+            {
+                matrix[0][j] = 0;
+            }
+        }
+        if (firstCol)
+        {
+            for (int i=0; i<n; i++)
+            {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+
+    private void print(int[][] grid)
+    {
+        for (int[] row : grid)
+        {
+            for (int num : row)
+            {
+                System.out.print(num+" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
