b2e3801d07d3c66138338882677f51907569d4f6
Dheeraj Gedupudi <102639070+DheerajGedupudi@users.noreply.github.com>
2025-07-24 20:57:20 -0500

    Time: 60 ms (49.03%), Space: 45.5 MB (11.13%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,11 +1,27 @@
 class Solution {
+    Map<Integer, Map<Integer, Integer>> memo;
+
     public int findTargetSumWays(int[] nums, int target) {
+        // this.counter = 0;
         int n = nums.length;
+        int sum = 0;
+        for (int x : nums)
+        {
+            sum += x;
+        }
+        this.memo = new HashMap<>();
         return helper(nums, 0, target);
     }
 
     private int helper(int[] nums, int index, int target)
     {
+        if (this.memo.containsKey(index))
+        {
+            if (this.memo.get(index).containsKey(target))
+            {
+                return this.memo.get(index).get(target);
+            }
+        }
         int n = nums.length;
         if (index==n)
         {
@@ -17,6 +33,8 @@ class Solution {
         }
         int x = helper(nums, index+1, target+nums[index]);
         int y = helper(nums, index+1, target-nums[index]);
+        this.memo.putIfAbsent(index, new HashMap<>());
+        this.memo.get(index).put(target, x+y);
         return x+y;
     }
 }
