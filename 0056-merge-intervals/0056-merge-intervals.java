732dbc32efe8fc37b2a715f5ddbbfa1683c4d98d
Dheeraj Gedupudi <102639070+DheerajGedupudi@users.noreply.github.com>
2025-07-31 13:29:50 -0500

    Time: 10 ms (21.19%), Space: 46.5 MB (72.73%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,31 +1,41 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a,b)->(a[0]!=b[0]?a[0]-b[0]:a[1]-b[1]));
-        LinkedList<int[]> list = new LinkedList<>();
-        int n = intervals.length;
-        list.addLast(intervals[0]);
-        for (int i=1; i<n; i++)
+        Arrays.sort(intervals, (a,b)->(a[0]!=b[0]?a[0]-b[0]:b[1]-a[1]));
+        List<int[]> list = new ArrayList<>();
+        for (int[] interval : intervals)
         {
-            int[] last = list.getLast();
-            if (last[1]>=intervals[i][0])
+            if (list.size()==0)
             {
-                int min = Math.min(intervals[i][0], last[0]);
-                int max = Math.max(intervals[i][1], last[1]);
-                list.removeLast();
-                list.addLast(new int[]{min, max});
+                list.add(interval);
+                continue;
+            }
+            int[] last = list.get(list.size()-1);
+            if (isOverlap(last, interval))
+            {
+                int end = Math.max(last[1], interval[1]);
+                last[1] = end;
             }
             else
             {
-                list.addLast(intervals[i]);
+                list.add(interval);
             }
+
         }
-        int size = list.size();
-        int[][] answer = new int[size][2];
-        for (int i=0; i<size; i++)
+        int[][] result = new int[list.size()][2];
+        for (int i=0; i<list.size(); i++)
         {
-            answer[i] = list.getFirst();
-            list.removeFirst();
+            result[i] = list.get(i);
         }
-        return answer;
+        return result;
+    }
+
+    private boolean isOverlap(int[] a, int[] b)
+    {
+        //a start <= b start
+        if (b[0]<=a[1])
+        {
+            return true;
+        }
+        return false;
     }
 }
