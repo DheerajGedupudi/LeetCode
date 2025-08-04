05b100e593340c72b462670810c0ce8be2d576f9
Dheeraj Gedupudi <102639070+DheerajGedupudi@users.noreply.github.com>
2025-08-04 16:33:36 -0500

    Time: 515 ms (16.91%), Space: 53.1 MB (11.57%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        Set<List<Integer>> tripletSet = new HashSet<>();
+        for (int i=0; i<n; i++)
+        {
+            int num = nums[i];
+            int target = 0-num;
+            int low = i+1;
+            int high = n-1;
+            while(low<high)
+            {
+                int sum = nums[low]+nums[high];
+                if (sum==target)
+                {
+                    List<Integer> triplet = new ArrayList<>();
+                    triplet.add(nums[i]);
+                    triplet.add(nums[low]);
+                    triplet.add(nums[high]);
+                    tripletSet.add(triplet);
+                    low++;
+                }
+                else if (sum>target)
+                {
+                    high--;
+                }
+                else
+                {
+                    low++;
+                }
+            }
+        }
+        return new ArrayList<>(tripletSet);
+    }
+}
