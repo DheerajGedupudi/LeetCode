c2306714eae3bb88ef6d4ec6ad99224bff36a9ac
Dheeraj Gedupudi <102639070+DheerajGedupudi@users.noreply.github.com>
2025-08-04 15:23:18 -0500

    Time: 6 ms (45.4%), Space: 121.5 MB (34.98%) - LeetHub

--- 2026-merge-triplets-to-form-target-triplet/2026-merge-triplets-to-form-target-triplet.java
+++ 2026-merge-triplets-to-form-target-triplet/2026-merge-triplets-to-form-target-triplet.java
@@ -6,13 +6,12 @@ class Solution {
         {
             int[] triplet2 = triplets[i];
             //usable?
-            if (triplet2[0]>target[0] || triplet2[1]>target[1] || triplet2[2]>target[2])
+            if (triplet2[0]<=target[0] && triplet2[1]<=target[1] && triplet2[2]<=target[2])
             {
-                continue;
+                triplet[0] = Math.max(triplet[0], triplet2[0]);
+                triplet[1] = Math.max(triplet[1], triplet2[1]);
+                triplet[2] = Math.max(triplet[2], triplet2[2]);
             }
-            triplet[0] = Math.max(triplet[0], triplet2[0]);
-            triplet[1] = Math.max(triplet[1], triplet2[1]);
-            triplet[2] = Math.max(triplet[2], triplet2[2]);
         }
         return triplet[0]==target[0] && triplet[1]==target[1] && triplet[2]==target[2];
     }
