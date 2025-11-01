a37818d66465bca684950bc805093306130c5c0e
Dheeraj Gedupudi <102639070+DheerajGedupudi@users.noreply.github.com>
2025-10-31 23:46:58 -0500

    Time: 19 ms (33.99%), Space: 51.3 MB (7.61%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,20 +1,39 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        Queue<Integer> heap = new PriorityQueue<>(Collections.reverseOrder());
         int n = matrix.length;
         int m = matrix[0].length;
+        Queue<Cell> heap = new PriorityQueue<>((a,b)->(matrix[a.row][a.col]-matrix[b.row][b.col]));
         for (int i=0; i<n; i++)
         {
-            for (int j=0; j<m; j++)
+            heap.offer(new Cell(i, 0));
+        }
+        int count = 0;
+        while(heap.isEmpty()==false)
+        {
+            count++;
+            Cell curr = heap.poll();
+            if (count==k)
             {
-                heap.offer(matrix[i][j]);
-                if (heap.size()>k)
-                {
-                    heap.poll();
-                }
+                return matrix[curr.row][curr.col];
+            }
+            if (curr.col+1<m)
+            {
+                heap.offer(new Cell(curr.row, curr.col+1));
             }
         }
-        return heap.peek();
+        return -1;
+    }
+}
+
+class Cell
+{
+    int row;
+    int col;
+
+    Cell(int row, int col)
+    {
+        this.row = row;
+        this.col = col;
     }
 }
 
