4e3b9b1f72b3e5cd7824855c6ebaf8fb89f3a572
Dheeraj Gedupudi <102639070+DheerajGedupudi@users.noreply.github.com>
2025-11-04 17:54:29 -0600

    Time: 26 ms (27.77%), Space: 69.4 MB (5.07%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.java
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -1,69 +1,79 @@
 class Solution {
     public int calculate(String s) {
-        int len = s.length();
-        int currNum = 0;
-        char sign = '+';
-        Deque<String> stack = new ArrayDeque<>();
-        for (int i=0; i<len; i++)
+        Deque<Integer> q1 = new LinkedList<>();
+        Deque<Character> q2 = new LinkedList<>();
+        Integer num = null;
+        for (char c : s.toCharArray())
         {
-            char c = s.charAt(i);
             if (c==' ')
             {
                 continue;
             }
-            //is num
             if (Character.isDigit(c))
             {
-                currNum *= 10;
-                currNum += Character.getNumericValue(c);
-            }
-            else if (c=='+'||c=='-'||c=='*'||c=='/')
-            {
-                //solve mult, and div
-                if (sign=='*'||sign=='/')
+                if (num==null)
                 {
-                    int result = 0;
-                    int lastNum = Integer.parseInt(stack.pop());
-                    if (sign=='*')
-                    {
-                        result = lastNum*currNum;
-                    }
-                    else
-                    {
-                        result = lastNum/currNum;
-                    }
-                    stack.push(""+result);
+                    num = 0;
                 }
-                else 
-                {
-                    stack.push(sign+""+currNum);
-                }
-                currNum = 0;
-                sign = c;
-            }
-        }
-        if (sign=='*'||sign=='/')
-        {
-            int result = 0;
-            int lastNum = Integer.parseInt(stack.pop());
-            if (sign=='*')
-            {
-                result = lastNum*currNum;
+                num *= 10;
+                num += Character.getNumericValue(c);
             }
             else
             {
-                result = lastNum/currNum;
+                if (num!=null)
+                {
+                    q1.offer(num);
+                }
+                num = null;
+                q2.offer(c);
             }
-            stack.push(""+result);
         }
-        else
+        if (num!=null)
         {
-            stack.push(sign+""+currNum);
+            q1.offer(num);
         }
-        int result = 0;
-        while(stack.isEmpty()==false)
+        // System.out.println(q1);
+        // System.out.println(q2);
+        //div and mul
+        Queue<Integer> q11 = new LinkedList<>();
+        Queue<Character> q22 = new LinkedList<>();
+        while(q1.size()>1)
         {
-            result += Integer.parseInt(stack.pop());
+            int op1 = q1.poll();
+            char sym = q2.poll();
+            int op2 = q1.poll();
+            if (sym=='/')
+            {
+                int x = op1/op2;
+                q1.offerFirst(x);
+            }
+            else if (sym=='*')
+            {
+                int x = op1*op2;
+                q1.offerFirst(x);
+            }
+            else
+            {
+                q11.offer(op1);
+                q22.offer(sym);
+                q1.offerFirst(op2);
+            }
+        }
+        q11.offer(q1.poll());
+        //now add and sub
+        int result = q11.poll();
+        while(q11.isEmpty()==false)
+        {
+            char sym = q22.poll();
+            int x = q11.poll();
+            if (sym=='+')
+            {
+                result += x;
+            }
+            else
+            {
+                result -= x;
+            }
         }
         return result;
     }
