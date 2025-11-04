cd1b2d2bcc90ef595e7eabfc66169521fb7a5026
Dheeraj Gedupudi <102639070+DheerajGedupudi@users.noreply.github.com>
2025-11-03 22:23:53 -0600

    Time: 103 ms (18.46%), Space: 123.8 MB (5.8%) - LeetHub

--- 2169-simple-bank-system/2169-simple-bank-system.java
+++ 2169-simple-bank-system/2169-simple-bank-system.java
@@ -1,62 +1,53 @@
 class Bank {
 
-    private long[] balance;
+    private long[] vault;
 
     public Bank(long[] balance) {
-        this.balance = balance; 
+        this.vault = balance;
     }
     
     public boolean transfer(int account1, int account2, long money) {
+        //check possibility
         account1--;
         account2--;
-        // System.out.println("transfer : "+account1+" -> "+account2);
-        int n = this.balance.length;
+        int n = this.vault.length;
         if (account1<0 || account1>=n || account2<0 || account2>=n)
         {
             return false;
         }
-        long currBalance = this.balance[account1];
-        if (money<=currBalance)
-        {
-            this.balance[account1] -= money;
-            this.balance[account2] += money;
-            return true;
-        }
-        else
+        if (this.vault[account1]<money)
         {
             return false;
         }
+        this.vault[account1] -= money;
+        this.vault[account2] += money;
+        return true;
     }
     
     public boolean deposit(int account, long money) {
         account--;
-        int n = this.balance.length;
+        int n = this.vault.length;
         if (account<0 || account>=n)
         {
             return false;
         }
-        this.balance[account] += money;
+        this.vault[account] += money;
         return true;
     }
     
     public boolean withdraw(int account, long money) {
         account--;
-        int n = this.balance.length;
+        int n = this.vault.length;
         if (account<0 || account>=n)
         {
             return false;
         }
-        long currBalance = this.balance[account];
-        if (money<=currBalance)
-        {
-            this.balance[account] -= money;
-            return true;
-        }
-        else
+        if (this.vault[account]<money)
         {
             return false;
         }
-        
+        this.vault[account] -= money;
+        return true;
     }
 }
 
