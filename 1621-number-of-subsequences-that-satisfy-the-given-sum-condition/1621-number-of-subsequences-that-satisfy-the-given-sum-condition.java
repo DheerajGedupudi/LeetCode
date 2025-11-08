36a4499c6aeb55ed91230094520ac963761a0222
Dheeraj Gedupudi <102639070+DheerajGedupudi@users.noreply.github.com>
2025-11-08 15:36:50 -0600

    Time: 55 ms (6.63%), Space: 70.9 MB (5.02%) - LeetHub

---
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/1621-number-of-subsequences-that-satisfy-the-given-sum-condition.java
@@ -0,0 +1,78 @@
+class Solution {
+
+    private int[] powers;
+    private int MOD;
+
+    public int numSubseq(int[] nums, int target) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        this.powers = new int[n+1];
+        this.MOD = (int)Math.pow(10, 9)+7;
+        long counter = 0;
+        for (int i=0; i<n; i++)
+        {
+            int x = nums[i];
+            int req = target-x;
+            //get largest less than or equal to req
+            int low = i;
+            int high = n-1;
+            int best = -1;
+            while(low<=high)
+            {
+                int mid = low + (high-low)/2;
+                if (nums[mid]<=req)
+                {
+                    best = mid;
+                    low = mid+1;
+                }
+                else
+                {
+                    high = mid-1;
+                }
+            }
+            if (best!=-1)
+            {
+                int found = best-i;
+                // System.out.println("for : "+i+" found : "+found);
+                counter += getPowerOf2(found);
+                counter %= MOD;
+            }
+
+        }
+        return (int)counter;
+    }
+
+    private int getPowerOf2(int n)
+    {
+        if (this.powers[n]!=0)
+        {
+            return this.powers[n];
+        }
+        if (n==1)
+        {
+            this.powers[1] = 2;
+            return 2;
+        }
+        if (n==0)
+        {
+            this.powers[0] = 1;
+            return 1;
+        }
+        int x = getPowerOf2(n-1);
+        int ans = x+x;
+        ans %= MOD;
+        this.powers[n] = ans;
+        return ans;
+
+    }
+}
+
+/*
+
+[3,3,6,8]
+
+
+3 -> 8 = 3->  
+7
+
+*/
