b3c18475d7eeff6e011d84d0eed3fcdd97dc202b
Dheeraj Gedupudi <102639070+DheerajGedupudi@users.noreply.github.com>
2025-11-26 17:11:14 -0600

    Time: 75 ms (53.1%), Space: 132.2 MB (10.18%) - LeetHub

--- 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.java
+++ 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.java
@@ -4,25 +4,29 @@ class Solution {
         int m = grid[0].length;
         int MOD = (int)Math.pow(10, 9)+7;
         int[][][] dp = new int[n+1][m+1][k];
+        int[][] currRow = new int[m+1][k];
+        int[][] prevRow = new int[m+1][k];
         for (int i=0; i<n; i++)
         {
             for (int j=0; j<m; j++)
             {
                 if (i==0 && j==0)
                 {
-                    dp[1][1][grid[0][0]%k] = 1;
+                    currRow[1][grid[0][0]%k] = 1;
                 }
                 else
                 {
                     for (int l=0; l<k; l++)
                     {
                         int pastIndex = (l+k-(grid[i][j]%k))%k;
-                        dp[i+1][j+1][l] = dp[i][j+1][pastIndex]+dp[i+1][j][pastIndex];
-                        dp[i+1][j+1][l] %= MOD;
+                        currRow[j+1][l] = prevRow[j+1][pastIndex]+currRow[j][pastIndex];
+                        currRow[j+1][l] %= MOD;
                     }
                 }
             }
+            prevRow = currRow;
+            currRow = new int[m+1][k];
         }
-        return dp[n][m][0];
+        return prevRow[m][0];
     }
 }
