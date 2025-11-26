23cedaac0a54e9ded820ca48f02006a0dc6070d1
Dheeraj Gedupudi <102639070+DheerajGedupudi@users.noreply.github.com>
2025-11-26 17:08:39 -0600

    Time: 69 ms (58.41%), Space: 98.8 MB (17.7%) - LeetHub

--- 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.java
+++ 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.java
@@ -1,67 +1,28 @@
 class Solution {
-
-    private int MOD;
-    private int[][][] memo;
-
     public int numberOfPaths(int[][] grid, int k) {
         int n = grid.length;
         int m = grid[0].length;
-        this.memo = new int[n][m][k];
+        int MOD = (int)Math.pow(10, 9)+7;
+        int[][][] dp = new int[n+1][m+1][k];
         for (int i=0; i<n; i++)
         {
             for (int j=0; j<m; j++)
             {
-                this.memo[i][j][0] = -1;
+                if (i==0 && j==0)
+                {
+                    dp[1][1][grid[0][0]%k] = 1;
+                }
+                else
+                {
+                    for (int l=0; l<k; l++)
+                    {
+                        int pastIndex = (l+k-(grid[i][j]%k))%k;
+                        dp[i+1][j+1][l] = dp[i][j+1][pastIndex]+dp[i+1][j][pastIndex];
+                        dp[i+1][j+1][l] %= MOD;
+                    }
+                }
             }
         }
-        this.MOD = (int)Math.pow(10, 9)+7;
-        int[] result = helper(grid, k, n-1, m-1);
-        return result[0];
-    }
-
-    //int[] = {0,1,2,...,k-1}
-    // ways to arrive at this sum % k
-
-    private int[] helper(int[][] grid, int k, int x, int y)
-    {
-        int n = grid.length;
-        int m = grid[0].length;
-        if (x<0 || y<0)
-        {
-            return new int[k];
-        }
-        if (this.memo[x][y][0]!=-1)
-        {
-            return this.memo[x][y];
-        }
-        if (x==0 && y==0)
-        {
-            int[] arr = new int[k];
-            arr[grid[0][0]%k] = 1;
-            return arr;
-        }
-        //from top
-        int[] top = helper(grid, k, x-1, y);
-        //from left
-        int[] left = helper(grid, k, x, y-1);
-        int[] ways = new int[k]; 
-        for (int i=0; i<k; i++)
-        {
-            int pastIndex = (i+k-(grid[x][y])%k)%k;
-            //from left
-            if (y-1>=0)
-            {
-                ways[i] += left[pastIndex];
-                ways[i] %= this.MOD;
-            }
-            //from top
-            if (x-1>=0)
-            {
-                ways[i] += top[pastIndex];
-                ways[i] %= this.MOD;
-            }
-        }
-        this.memo[x][y] = ways;
-        return ways;
+        return dp[n][m][0];
     }
 }
