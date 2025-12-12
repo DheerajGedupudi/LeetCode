71328366063b696644ec40ff03e8339762f764f0
Dheeraj Gedupudi <102639070+DheerajGedupudi@users.noreply.github.com>
2025-12-12 15:18:34 -0600

    Time: 6 ms (74.7%), Space: 104.6 MB (12.65%) - LeetHub

--- 1874-minimize-product-sum-of-two-arrays/1874-minimize-product-sum-of-two-arrays.java
+++ 1874-minimize-product-sum-of-two-arrays/1874-minimize-product-sum-of-two-arrays.java
@@ -1,7 +1,7 @@
 class Solution {
     public int minProductSum(int[] nums1, int[] nums2) {
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
+        countSort(nums1);
+        countSort(nums2);
         int result = 0;
         int n = nums1.length;
         for (int i=0; i<n; i++)
@@ -9,5 +9,25 @@ class Solution {
             result += (nums1[i]*nums2[n-1-i]);
         }
         return result;
+    }
+
+    private void countSort(int[] nums)
+    {
+        int[] count = new int[101];
+        for (int x : nums)
+        {
+            count[x]++;
+        }
+        int number = 0;
+        int index = 0;
+        while(number<=100)
+        {
+            while(count[number]>0)
+            {
+                nums[index++] = number;
+                count[number]--;
+            }
+            number++;
+        }
     }
 }
