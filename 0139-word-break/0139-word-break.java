14ea2da476629762629f00075a90b90dce5edb7a
Dheeraj Gedupudi <102639070+DheerajGedupudi@users.noreply.github.com>
2025-12-18 15:16:02 -0600

    Time: 1 ms (99.54%), Space: 42.7 MB (90.43%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,74 +1,62 @@
 class Solution {
+
+    private Boolean[] memo;
+
     public boolean wordBreak(String s, List<String> wordDict) {
-        Trie trie = new Trie();
+        this.memo = new Boolean[s.length()+1];
+        return helper(s, wordDict, 0);
+    }
+
+    private boolean helper(String s, List<String> wordDict, int index)
+    {
+        if (this.memo[index]!=null)
+        {
+            return this.memo[index];
+        }
+        int len = s.length();
+        if (index==len)
+        {
+            this.memo[index] = true;
+            return true;
+        }
         for (String word : wordDict)
         {
-            trie.add(word);
-        }
-        int n = s.length();
-        boolean[] dp = new boolean[n];
-        for (int end=0; end<n; end++)
-        {
-            for (int start=0; start<=end; start++)
+            if (check(s, word, index))
             {
-                if ( (start==0 || dp[start-1]) && trie.contains(s, start, end))
+                // System.out.println("possible : "+s.substring(index)+" used : "+word);
+                if (helper(s, wordDict, index+word.length()))
                 {
-                    dp[end] = true;
+                    this.memo[index] = true;
+                    return true;
                 }
             }
         }
-        return dp[n-1];
-    }
-}
-
-class Trie
-{
-    TrieNode root;
-
-    Trie()
-    {
-        this.root = new TrieNode();
+        this.memo[index] = false;
+        return false;
     }
 
-    void add(String s)
+    private boolean check(String s, String word, int index)
     {
-        TrieNode curr = this.root;
-        for (char c : s.toCharArray())
+        int len = s.length();
+        int len2 = index+word.length();
+        if (len2<=len)
         {
-            int index = c-'a';
-            if (curr.children[index]==null)
+            for (int i=0; i<word.length(); i++)
             {
-                curr.children[index] = new TrieNode();
+                if (s.charAt(i+index)!=word.charAt(i))
+                {
+                    return false;
+                }
             }
-            curr = curr.children[index];
+            return true;
         }
-        curr.flag = true;
-    }
-
-    boolean contains(String s, int start, int end)
-    {
-        TrieNode curr = this.root;
-        for (int i=start; i<=end; i++)
-        {
-            int index = s.charAt(i)-'a';
-            if (curr.children[index]==null)
-            {
-                return false;
-            }
-            curr = curr.children[index];
-        }
-        return curr.flag;
+        return false;
     }
 }
 
-class TrieNode
-{
-    TrieNode[] children;
-    boolean flag;
+/*
 
-    TrieNode()
-    {
-        this.children = new TrieNode[26];
-        this.flag = false;
-    }   
-}
+ catsandog
+       dog
+
+*/
