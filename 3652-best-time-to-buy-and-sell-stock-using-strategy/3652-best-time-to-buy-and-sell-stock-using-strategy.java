8696ee066daae650b0c30762e798e3071a202083
Dheeraj Gedupudi <102639070+DheerajGedupudi@users.noreply.github.com>
2025-12-18 14:31:15 -0600

    Time: 9 ms (7.38%), Space: 98 MB (53.36%) - LeetHub

--- 3652-best-time-to-buy-and-sell-stock-using-strategy/3652-best-time-to-buy-and-sell-stock-using-strategy.java
+++ 3652-best-time-to-buy-and-sell-stock-using-strategy/3652-best-time-to-buy-and-sell-stock-using-strategy.java
@@ -1,12 +1,13 @@
 class Solution {
     public long maxProfit(int[] prices, int[] strategy, int k) {
-        int n = prices.length;
+        long max = Long.MIN_VALUE;
         long sum = 0;
+        int n = prices.length;
         for (int i=0; i<n; i++)
         {
             sum += (long)prices[i]*strategy[i];
         }
-        long max = sum;
+        max = Math.max(max, sum);
         int[] strat = Arrays.copyOf(strategy, n);
         for (int i=0; i<k; i++)
         {
@@ -19,51 +20,36 @@ class Solution {
                 strat[i] = 1;
             }
         }
-        // System.out.println(Arrays.toString(strategy)+" "+sum);
-        sum = 0;
         for (int i=0; i<n; i++)
         {
+            sum -= (long)prices[i]*strategy[i];
             sum += (long)prices[i]*strat[i];
         }
-        // System.out.println(Arrays.toString(strat)+" "+sum);
         max = Math.max(max, sum);
+        // System.out.println(Arrays.toString(strat)+" -> "+sum);
         for (int i=0; i+k<n; i++)
         {
-            //update w
-            int index = i;
-            sum -= (long)prices[index]*strat[index];
-            strat[index] = strategy[index];
-            sum += (long)prices[index]*strat[index];
+            //i, make original
+            //i+k/2, make 0
+            //i+k, make 1
+            int ind = i;
+            sum -= (long)prices[ind]*strat[ind];
+            strat[ind] = strategy[ind];
+            sum += (long)prices[ind]*strat[ind];
 
-            //update w + k/2
-            index = i+k/2;
-            sum -= (long)prices[index]*strat[index];
-            strat[index] = 0;
-            sum += (long)prices[index]*strat[index];
-
-            //update w + k
-            index = i+k;
-            sum -= (long)prices[index]*strat[index];
-            strat[index] = 1;
-            sum += (long)prices[index]*strat[index];
-            // System.out.println(Arrays.toString(strat)+" "+sum);
+            ind = i+k/2;
+            sum -= (long)prices[ind]*strat[ind];
+            strat[ind] = 0;
+            sum += (long)prices[ind]*strat[ind];
+            
+            ind = i+k;
+            sum -= (long)prices[ind]*strat[ind];
+            strat[ind] = 1;
+            sum += (long)prices[ind]*strat[ind];
 
             max = Math.max(max, sum);
+            // System.out.println(Arrays.toString(strat)+" -> "+sum);
         }
         return max;
     }
 }
-
-
-/*
-
-k = 4
-k/2 = 2
-
-[0,1,2,3,4,5,6,7,8,9,0]
-[0,0,1,1]
-
-
-
-
-*/
