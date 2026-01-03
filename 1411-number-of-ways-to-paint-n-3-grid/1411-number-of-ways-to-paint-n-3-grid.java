36b4e11dbac3646b778d2a9e8df99dbe7dd55b71
Dheeraj Gedupudi <102639070+DheerajGedupudi@users.noreply.github.com>
2026-01-03 13:35:39 -0600

    Time: 64 ms (16.58%), Space: 61.8 MB (5.18%) - LeetHub

--- 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.java
+++ 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.java
@@ -1,45 +1,41 @@
 class Solution {
+
+    private int MOD;
+    private Integer[][][][] memo;
+
     public int numOfWays(int n) {
-        int MOD = (int)Math.pow(10, 9)+7;
-        long aba = 6;
-        long abc = 6;
-        for (int i=1; i<n; i++)
+        this.MOD = (int)Math.pow(10, 9)+7;
+        this.memo = new Integer[n][4][4][4];
+        return helper(n, 0, 0, 0, 0);
+    }
+
+    private int helper(int n, int row, int prev1, int prev2, int prev3)
+    {
+        if (row==n)
         {
-            long resultABA = (aba*3) + (abc*2);
-            long resultABC = (aba*2) + (abc*2);
-            aba = resultABA % MOD;
-            abc = resultABC % MOD;
+            return 1;
         }
-        return (int)((aba+abc)%MOD);
+        if (this.memo[row][prev1][prev2][prev3]!=null)
+        {
+            return this.memo[row][prev1][prev2][prev3];
+        }
+        long sum = 0;
+        for (int col1=1; col1<=3; col1++)
+        {
+            if (col1==prev1) continue;
+            for (int col2=1; col2<=3; col2++)
+            {
+                if (col2==prev2) continue;
+                if (col1==col2) continue;
+                for (int col3=1; col3<=3; col3++)
+                {
+                    if (col3==prev3) continue;
+                    if (col2==col3) continue;
+                    sum += helper(n, row+1, col1, col2, col3)%this.MOD;
+                }
+            }
+        }
+        this.memo[row][prev1][prev2][prev3] = (int)(sum%this.MOD);
+        return this.memo[row][prev1][prev2][prev3];
     }
 }
-
-/*
-
-rgb
-
-
-aba
-
-rgr 01
-rbr 02
-grg 03
-gbg 04
-brb 05
-bgb 06
-
-abc
-
-rgb 11
-rbg 12
-grb 13
-gbr 14
-brg 15
-bgr 16
-
-
-for 01, we have 03, 04, 05, 13, 15
-
-for 11, we have 03, 04, 14, 15
-
-*/
