9dba832e45ed5337671798cb2d1b1815b1377039
Dheeraj Gedupudi <102639070+DheerajGedupudi@users.noreply.github.com>
2026-01-13 00:20:22 -0600

    Time: 191 ms (20%), Space: 148.8 MB (44.74%) - LeetHub

--- 3453-separate-squares-i/3453-separate-squares-i.java
+++ 3453-separate-squares-i/3453-separate-squares-i.java
@@ -2,13 +2,23 @@ class Solution {
     public double separateSquares(int[][] squares) {
         double low = 0;
         double high = Math.pow(10, 9)+10;
-        double tolerance = Math.pow(10, -5);
+        double precision = Math.pow(10, -5);
         double answer = 0;
-        while(high-low>=tolerance)
+        double area = 0;
+        for (int[] sq : squares)
+        {
+            double xi = sq[0];
+            double yi = sq[1];
+            double li = sq[2];
+            double top = yi+li;
+            double bot = yi;
+            area += ((top-bot)*li);
+        }
+        while(high-low>=precision)
         {
             double mid = low + (high-low)/2;
-            double areaRatio = get(squares, mid);
-            if (areaRatio>1)
+            double areaUp = get(squares, mid);
+            if (areaUp>area/2)
             {
                 low = mid;
             }
@@ -24,7 +34,6 @@ class Solution {
     private double get(int[][] squares, double y)
     {
         double areaUp = 0;
-        double areaDown = 0;
         for (int[] sq : squares)
         {
             double xi = sq[0];
@@ -33,17 +42,12 @@ class Solution {
             double top = yi+li;
             double bot = yi;
             double up = top-Math.max(y, bot);
-            double down = Math.min(top, y)-bot;
             if (up>0)
             {
                 areaUp += (up*li);
             }
-            if (down>0)
-            {
-                areaDown += (down*li);
-            }
         }
-        return areaUp/areaDown;
+        return areaUp;
 
     }
 }
