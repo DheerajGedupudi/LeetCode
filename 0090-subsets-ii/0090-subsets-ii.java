35fee077501711e44178bf74d3b2ed9f4ecd4da8
Dheeraj Gedupudi <102639070+DheerajGedupudi@users.noreply.github.com>
2026-02-24 10:19:20 -0600

    Time: 3 ms (55.08%), Space: 45.4 MB (23.56%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -2,17 +2,24 @@ class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
         int len = nums.length;
-        Set<List<Integer>> result = new HashSet<>();
+        List<List<Integer>> result = new ArrayList<>();
         for (int i=0; i<(1<<len); i++)
         {
             List<Integer> list = new ArrayList<>();
+            boolean flag = true;
             for (int bit=0; bit<len; bit++)
             {
                 if ((i&(1<<bit))!=0)
                 {
+                    if (bit>0 && nums[bit]==nums[bit-1] && (i&(1<<(bit-1)))==0)
+                    {
+                        flag = false;
+                        break;
+                    }
                     list.add(nums[bit]);
                 }
             }
+            if (flag)
             result.add(list);
         }
         return new ArrayList<>(result);
