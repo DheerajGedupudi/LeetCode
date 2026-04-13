c3d032e05c4bef2b440658f758fbf69d5467e0ab
Dheeraj Gedupudi <102639070+DheerajGedupudi@users.noreply.github.com>
2026-04-13 17:10:17 -0500

    Time: 3 ms (52.87%), Space: 20.5 MB (25.9%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        indexes_hashed = {}
+        for i, x in enumerate(nums):
+            required = target-x
+            if required in indexes_hashed.keys():
+                return [indexes_hashed[required], i]
+            indexes_hashed[x] = i
+        return []
+        
