f0417c3142a9fe636856bea54d0dc6aaa779d418
Dheeraj Gedupudi <102639070+DheerajGedupudi@users.noreply.github.com>
2026-04-13 23:43:28 -0500

    Time: 0 ms (100%), Space: 19.5 MB (18.41%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -0,0 +1,48 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if len(nums) == 1:
+            if nums[0] == target:
+                return 0
+            else:
+                return -1
+        intersect = 0
+        low = 1 # look for possibilities where it is actually rotated
+        high = len(nums)-1
+        while low <= high:
+            mid = low + (high-low)//2
+            if mid < 0:
+                break
+            if nums[mid] > nums[(mid+1)%len(nums)]:
+                # at intersect
+                intersect = mid
+                break
+            else:
+                if nums[mid]>nums[0] and nums[mid]>nums[-1]:
+                    # 1st half
+                    low = mid+1
+                else:
+                    # 2nd half
+                    high = mid-1
+        result = self.bin_search(nums, target, 0, intersect)
+        if not result==-1:
+            return result
+        result = self.bin_search(nums, target, intersect+1, len(nums)-1)
+        if not result==-1:
+            return result
+        return -1
+        
+    
+
+    def bin_search(self, nums, target, start, end) -> int:
+        low, high = start, end
+        while low <= high:
+            mid = low + (high-low)//2
+            if mid < 0:
+                return -1
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                low = mid+1
+            else:
+                high = mid-1
+        return -1
