735788efb17b999fb83a90fc337b7c26a9924893
Dheeraj Gedupudi <102639070+DheerajGedupudi@users.noreply.github.com>
2026-04-14 00:46:32 -0500

    Time: 0 ms (100%), Space: 19.6 MB (12.6%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,20 +1,4 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        result = []
-
-        def backtrack(nums, index, path):
-            if index == len(nums):
-                result.append(list(path))
-                return
-            # Choose
-            path.append(nums[index])
-            backtrack(nums, index+1, path)
-            path.pop()
-            # don't choose
-            backtrack(nums, index+1, path)
-            
-        backtrack(nums, 0, [])
-        return result
-
-
+        return [list(combs) for x in range(len(nums)+1) for combs in combinations(nums, x)]
         
