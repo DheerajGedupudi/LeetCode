a46dac60597c7c865f882c1cdde06ea478d2e049
Dheeraj Gedupudi <102639070+DheerajGedupudi@users.noreply.github.com>
2026-04-14 00:30:29 -0500

    Time: 54 ms (76.84%), Space: 24.5 MB (70.27%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,8 +1,3 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        return heapq.nsmallest(k, points, key=lambda p : self.dist_squared(p[0], p[1]))
-            
-
-
-    def dist_squared(self, x, y):
-        return x**2 + y**2
+        return heapq.nsmallest(k, points, key=lambda p : p[0]**2 + p[1]**2)
