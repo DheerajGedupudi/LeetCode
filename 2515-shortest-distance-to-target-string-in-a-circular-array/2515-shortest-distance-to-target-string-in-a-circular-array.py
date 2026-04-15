d2ec2f0477938caebd448a5b01d647bd04007b6f
Dheeraj Gedupudi <102639070+DheerajGedupudi@users.noreply.github.com>
2026-04-15 11:39:29 -0500

    Time: 0 ms (100%), Space: 19.4 MB (52.69%) - LeetHub

--- 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.py
+++ 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.py
@@ -1,18 +1,17 @@
 class Solution:
     def closestTarget(self, words: List[str], target: str, startIndex: int) -> int:
         n = len(words)
-        closest = n+1
+        closest = math.inf
         def get_dist(index):
             # left
             left = (index - startIndex + n) % n
             # right
             right = (startIndex - index + n) % n
-            # print(f"from {index}: {words[index]}, left : {left}, right : {right}")
-            return min (left, right)
+            return min(left, right)
 
         for i, x in enumerate(words):
             if x == target:
                 closest = min(closest, get_dist(i))
-        if closest > n:
+        if closest is math.inf:
             return -1
         return closest
