716ee8ca5c94bd478868c3504242bef152edfee8
Dheeraj Gedupudi <102639070+DheerajGedupudi@users.noreply.github.com>
2026-04-28 14:14:26 -0500

    Time: 186 ms (13.75%), Space: 46.7 MB (98.13%) - LeetHub

---
+++ 0943-find-the-shortest-superstring/0943-find-the-shortest-superstring.java
@@ -0,0 +1,103 @@
+class Solution {
+
+    private Integer[][] memo;
+    private int[][] chosen;
+    private int INF;
+
+    public String shortestSuperstring(String[] words) {
+        int n = words.length;
+        this.memo = new Integer[(1<<n)][n];
+        this.chosen = new int[(1<<n)][n];
+        this.INF = (int)Math.pow(10,9);
+        int lastIndex = 0;
+        int min = this.INF;
+        for (int i=0; i<n; i++)
+        {
+            int totalLen = helper(words, (1<<i), i) + words[i].length();
+            if (totalLen < min)
+            {
+                min = totalLen;
+                lastIndex = i;
+            }
+        }
+        // System.out.println(min);
+        StringBuilder sb = new StringBuilder();
+        sb.append(words[lastIndex]);
+        int mask = (1<<lastIndex);
+        int fullMask = (1<<n)-1;
+        while(mask != fullMask)
+        {
+            int index = this.chosen[mask][lastIndex];
+            int overlap = getMaxOverlap(words[lastIndex], words[index]);
+            sb.append(words[index].substring(overlap));
+            mask |= (1<<index);
+            lastIndex = index;
+        }
+        return sb.toString();
+    }
+
+    private int helper(String[] words, int currMask, int lastIndex)
+    {
+        if (this.memo[currMask][lastIndex]!=null)
+        {
+            return this.memo[currMask][lastIndex];
+        }
+        int n = words.length;
+        if (currMask==(1<<n)-1)
+        {
+            return 0;
+        }
+        int min = this.INF;
+        String lastW = words[lastIndex];
+        for (int i=0; i<n; i++)
+        {
+            String currW = words[i];
+            if ((currMask & (1<<i))==0)
+            {
+                int union = currMask | (1<<i);
+                int additionalSpace = currW.length()-(getMaxOverlap(lastW, currW));
+                int cost = additionalSpace + helper(words, union, i);
+                if (cost < min)
+                {
+                    this.chosen[currMask][lastIndex] = i;
+                    min = cost;
+                }
+            }
+        }
+        this.memo[currMask][lastIndex] = min;
+        return min;
+    }
+
+    private int getMaxOverlap(String prev, String curr)
+    {
+        int n = prev.length();
+        int m = curr.length();
+        int max = 0;
+        int start = Math.max(0, (n-m));
+        for (int i=start; i<n; i++)
+        {
+            int saved = getOverlapIndexes(prev, curr, i, 0);
+            max = Math.max(max, saved);
+        }
+        // System.out.println(prev+" - "+curr+" saved : "+max);
+        return max;
+    }
+
+    private int getOverlapIndexes(String s, String t, int i, int j)
+    {
+        int n = s.length();
+        int m = t.length();
+        int count = 0;
+        while(i<n && j<m)
+        {
+            if (s.charAt(i)!=t.charAt(j))
+            {
+                return 0;
+            }
+            i++;
+            j++;
+            count++;
+        }
+        return count;
+    }
+}
